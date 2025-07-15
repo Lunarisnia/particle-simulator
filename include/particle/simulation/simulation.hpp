1bc6d23e4c4e6aaa524327d364c98ca36173178d
Lounarisnia <r.arswendo.r@gmail.com>
2025-07-15 19:24:17 +0700

    feat: multi object initiation

--- include/particle/simulation/simulation.hpp
+++ include/particle/simulation/simulation.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include <vector>
 #include "core/object/object.hpp"
 #include "glm/ext/vector_float3.hpp"
 namespace Particle {
@@ -7,6 +8,9 @@ class Simulation {
  public:
   static float globalFloat;
   static glm::vec3 lightColor;
+  static std::vector<std::shared_ptr<Core::Object>> cubes;
+  static std::vector<std::shared_ptr<Core::Object>> lightCubes;
+
   static std::shared_ptr<Core::Object> cube;
   static std::shared_ptr<Core::Object> lightCube;
 
--- src/particle/simulation/simulation.cpp
+++ src/particle/simulation/simulation.cpp
@@ -1,6 +1,10 @@
 #include "particle/simulation/simulation.hpp"
+#include <cstddef>
+#include <format>
 #include <memory>
+#include <random>
 #include <string>
+#include <vector>
 #include "GLFW/glfw3.h"
 #include "core/components/rigidbody2d.hpp"
 #include "core/input/input.hpp"
@@ -8,11 +12,15 @@
 #include "core/static_camera/static_camera.hpp"
 #include "glm/ext/vector_float3.hpp"
 #include "glm/ext/vector_float4.hpp"
+#include "glm/trigonometric.hpp"
 #include "particle/primitive/primitive.hpp"
 
 float Particle::Simulation::globalFloat = 0.0f;
 std::shared_ptr<Core::Object> Particle::Simulation::cube;
 std::shared_ptr<Core::Object> Particle::Simulation::lightCube;
+std::vector<std::shared_ptr<Core::Object>> Particle::Simulation::cubes;
+std::vector<std::shared_ptr<Core::Object>> Particle::Simulation::lightCubes;
+
 glm::vec3 Particle::Simulation::lightColor = glm::vec3(1.0f);
 
 const std::string vertexPath = "./shaders/diffuse/diffuse.vert";
@@ -26,41 +34,57 @@ void Particle::Simulation::Init() {
 
   lightCube = Primitive::CreateCube(vertexPath, lightFrag);
   lightCube->transform->position = glm::vec3(1.5f, 1.5f, -1.5f);
-  lightCube->name = "Test2";
+  lightCube->name = "PointLight1";
 
-  cube = Primitive::CreateCube(vertexPath, diffuseFrag2);
-  cube->name = "Test1";
-  cube->mesh->material->LoadTexture("./assets/container2.png", GL_TEXTURE0,
-                                    GL_RGBA);
-  cube->mesh->material->LoadTexture("./assets/container2_specular.png",
-                                    GL_TEXTURE1, GL_RGBA);
-  cube->mesh->material->LoadTexture("./assets/matrix.jpg", GL_TEXTURE2, GL_RGB);
-  cube->transform->position.z = -1.0f;
-  cube->mesh->material->SetVec3("objectColor", glm::vec3(0.3f, 0.8f, 0.2f));
-  cube->mesh->material->SetInt("material.diffuse", 0);
-  cube->mesh->material->SetInt("material.specular", 1);
-  cube->mesh->material->SetInt("material.emission", 2);
-  cube->mesh->material->SetFloat("material.shininess", 36.0f);
+  /*cube = Primitive::CreateCube(vertexPath, diffuseFrag2);*/
+  /*cube->name = "BetaCube";*/
 
-  cube->mesh->material->SetVec3("directionalLight.direction",
-                                glm::normalize(glm::vec3(1.0f, -1.0f, 0.0f)));
-  cube->mesh->material->SetVec3("directionalLight.ambient", glm::vec3(0.5f));
-  cube->mesh->material->SetVec3("directionalLight.diffuse", glm::vec3(1.0f));
-  cube->mesh->material->SetVec3("directionalLight.specular", glm::vec3(1.0f));
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_real_distribution<float> dist(
+      -3.5f, 3.5f);  // distribution in range [1, 6]
 
-  cube->mesh->material->SetVec3("pointLight.ambient", glm::vec3(0.5f));
-  cube->mesh->material->SetVec3("pointLight.diffuse", glm::vec3(1.0f));
-  cube->mesh->material->SetVec3("pointLight.specular", glm::vec3(1.0f));
+  for (size_t i = 0; i < 10; i++) {
+    std::shared_ptr<Core::Object> obj =
+        Primitive::CreateCube(vertexPath, diffuseFrag2);
+    obj->name = std::format("Cube ({})", i);
+    obj->mesh->material->LoadTexture("./assets/container2.png", GL_TEXTURE0,
+                                     GL_RGBA);
+    obj->mesh->material->LoadTexture("./assets/container2_specular.png",
+                                     GL_TEXTURE1, GL_RGBA);
+    obj->mesh->material->LoadTexture("./assets/matrix.jpg", GL_TEXTURE2,
+                                     GL_RGB);
+    /*obj->transform->position.z = -1.0f;*/
+    obj->transform->position = glm::vec3(dist(rng), dist(rng), -2.0f);
 
-  /*cube->mesh->material->SetVec3("light.ambient", glm::vec3(1.0f));*/
-  /*cube->mesh->material->SetVec3("light.specular", glm::vec3(1.0f));*/
-  /*cube->mesh->material->SetFloat("light.constant", 1.0f);*/
-  /*cube->mesh->material->SetFloat("light.linear", 0.14f);*/
-  /*cube->mesh->material->SetFloat("light.quadratic", 0.07f);*/
-  /*cube->mesh->material->SetVec3("light.spotDirection",*/
-  /*                              glm::vec3(0.0f, -1.0f, 0.0f));*/
-  /*cube->mesh->material->SetFloat("light.cutoff", glm::radians(45.0f));*/
-  /*cube->mesh->material->SetFloat("light.outerCutoff", glm::radians(50.0f));*/
+    obj->mesh->material->SetVec3("objectColor", glm::vec3(0.3f, 0.8f, 0.2f));
+    obj->mesh->material->SetInt("material.diffuse", 0);
+    obj->mesh->material->SetInt("material.specular", 1);
+    obj->mesh->material->SetInt("material.emission", 2);
+    obj->mesh->material->SetFloat("material.shininess", 36.0f);
+
+    obj->mesh->material->SetVec3("directionalLight.direction",
+                                 glm::normalize(glm::vec3(1.0f, -1.0f, 0.0f)));
+    obj->mesh->material->SetVec3("directionalLight.ambient", glm::vec3(0.5f));
+    obj->mesh->material->SetVec3("directionalLight.diffuse", glm::vec3(1.0f));
+    obj->mesh->material->SetVec3("directionalLight.specular", glm::vec3(1.0f));
+
+    obj->mesh->material->SetVec3("pointLight.ambient", glm::vec3(0.5f));
+    obj->mesh->material->SetVec3("pointLight.diffuse", glm::vec3(1.0f));
+    obj->mesh->material->SetVec3("pointLight.specular", glm::vec3(1.0f));
+
+    /*obj->mesh->material->SetVec3("light.ambient", glm::vec3(1.0f));*/
+    /*obj->mesh->material->SetVec3("light.specular", glm::vec3(1.0f));*/
+    /*obj->mesh->material->SetFloat("light.constant", 1.0f);*/
+    /*obj->mesh->material->SetFloat("light.linear", 0.14f);*/
+    /*obj->mesh->material->SetFloat("light.quadratic", 0.07f);*/
+    /*obj->mesh->material->SetVec3("light.spotDirection",*/
+    /*                             glm::vec3(0.0f, -1.0f, 0.0f));*/
+    /*obj->mesh->material->SetFloat("light.cutoff", glm::radians(45.0f));*/
+    /*obj->mesh->material->SetFloat("light.outerCutoff", glm::radians(50.0f));*/
+
+    cubes.emplace_back(obj);
+  }
 }
 
 void Particle::Simulation::Update() {
@@ -73,12 +97,13 @@ void Particle::Simulation::Update() {
 
   // TODO: add stbi_image and texture support
   lightCube->mesh->material->SetVec3("lightColor", lightColor);
-
-  cube->mesh->material->SetVec3("pointLight.position", lightPosWorldSpace);
-  cube->mesh->material->SetVec3("light.position", lightPosWorldSpace);
-  cube->mesh->material->SetVec3("cameraPosition", cameraWorldSpace);
-  cube->mesh->material->SetFloat("globalFloat", globalFloat);
-  cube->mesh->material->SetVec3("light.diffuse", lightColor);
+  for (std::shared_ptr<Core::Object> &cube : cubes) {
+    cube->mesh->material->SetVec3("pointLight.position", lightPosWorldSpace);
+    cube->mesh->material->SetVec3("light.position", lightPosWorldSpace);
+    cube->mesh->material->SetVec3("cameraPosition", cameraWorldSpace);
+    cube->mesh->material->SetFloat("globalFloat", globalFloat);
+    cube->mesh->material->SetVec3("light.diffuse", lightColor);
+  }
 
   if (Core::Input::GetKey(GLFW_KEY_F)) {
     std::shared_ptr<Core::Object> obj =
